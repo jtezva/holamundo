b396f07b4b162b36343d1dbf4550b079af4775db
cesar1891 <cesar_18-91@hotmail.com>
2017-04-27 11:51:28 -0500

    Update Main.java
    git eclipse identado

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
-public static void main (String[] args) {
-Alvaro.alvaro();
-Cesar.cesar();
+  public static void main (String[] args) {
+    Alvaro.alvaro();
+    Cesar.cesar();
 Hector.hector();
 Ricardo.ricardo();
 }
