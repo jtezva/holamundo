f87f587cd44838f68ea823d9287a3ca9bb65c22c
erick.ortiz <Erick.Ortiz@segurossura.com.mx>
2017-04-27 12:47:43 -0500

    Se agrega llamado a clase de Erick

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
-public static void main (String[] args) {
-Alvaro.alvaro();
-Cesar.cesar();
-Hector.hector();
-Ricardo.ricardo();
-}
+    public static void main (String[] args) {
+        Alvaro.alvaro();
+        Cesar.cesar();
+        Hector.hector();
+        Ricardo.ricardo();
+        Erick.erick();
+    }
 }
